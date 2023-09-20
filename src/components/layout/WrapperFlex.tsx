9a7ee7b9db068cf52463bcd4f716d534584a27dd
u-dani <danibezsouza@gmail.com>
2023-09-20 08:11:41 -0300

    WrapperFlex created

---
+++ src/components/layout/WrapperFlex.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+interface IWrapperFlexProps {
+  width?: string
+  height?: string
+  direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse'
+  gap?: string
+  justifyContent?:
+    | 'start'
+    | 'center'
+    | 'end'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evelyn'
+    | 'stretch'
+  alignItems?: 'start' | 'center' | 'end' | 'stretch'
+  wrap?: boolean
+}
+
+export const WrapperFlex = styled.div<IWrapperFlexProps>`
+  width: ${props => props.width ?? '100%'};
+  height: ${props => props.height ?? '100%'};
+  display: flex;
+  flex-direction: ${props => props.direction ?? 'row'};
+  gap: ${props => props.gap ?? '0px'};
+  justify-content: ${props => props.justifyContent ?? 'center'};
+  align-items: ${props => props.justifyContent ?? 'center'};
+  flex-wrap: ${props => (props.wrap ? 'wrap' : 'nowrap')};
+`
