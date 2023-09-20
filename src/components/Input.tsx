7a5337b4da6a377b223d0ea6cbf68fc58a924cfc
u-dani <danibezsouza@gmail.com>
2023-09-20 03:25:43 -0300

    changing style
    changing the padding to 12px and adding outline style to the focus

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,12 +7,20 @@ interface InputProps {
 }
 
 export const Input = styled.input<InputProps>`
-  border-radius: 6px;
+  border-radius: 4px;
   border: 1px solid transparent;
   color: #333;
   font-size: ${props => props.fontSize || '1.2rem'};
   outline: none;
-  padding: 8px;
+  padding: 12px;
   text-align: ${props => props.textAlign || 'start'};
   width: ${props => props.width || '100%'};
+  background-color: rgba(255, 255, 255, 0.9);
+
+  &:focus {
+    background: transparent;
+    border-color: rgba(255, 255, 255, 0.5);
+    color: rgba(255, 255, 255, 0.9);
+    font-weight: normal;
+  }
 `
