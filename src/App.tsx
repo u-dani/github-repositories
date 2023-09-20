c6da3d7bf3bd4717a963249d7732bbb8b1ee5d8c
u-dani <danibezsouza@gmail.com>
2023-09-20 09:56:10 -0300

    `app` page created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Button } from './components/form/Button'
+import { Input } from './components/form/Input'
+import { WrapperFlex } from './components/layout/WrapperFlex'
+import { Text } from './components/Text'
+
 function App() {
-  return <main></main>
+  return (
+    <WrapperFlex direction='column' gap='40px' height='100vh' padding='40px'>
+      <Text size='xxl' align='center' weight='bold'>
+        Repositórios do Github
+      </Text>
+      <Input
+        placeholder='Digite o usuário do Github'
+        textAlign='center'
+        maxWidth='600px'
+      />
+
+      <Button width='160px'>Buscar</Button>
+    </WrapperFlex>
+  )
 }
 
 export default App
