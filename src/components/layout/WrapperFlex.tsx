1ad346e571518635b7aadc628c61c616bbfefd2c
u-dani <danibezsouza@gmail.com>
2023-09-24 19:17:08 -0300

    fixing the alignItems property

--- src/components/layout/WrapperFlex.tsx
+++ src/components/layout/WrapperFlex.tsx
@@ -20,12 +20,12 @@ interface IWrapperFlexProps {
 
 export const WrapperFlex = styled.div<IWrapperFlexProps>`
   width: ${props => props.width ?? '100%'};
-  height: ${props => props.height ?? '100%'};
+  height: ${props => props.height ?? 'auto'};
   display: flex;
   flex-direction: ${props => props.direction ?? 'row'};
   gap: ${props => props.gap ?? '0px'};
   justify-content: ${props => props.justifyContent ?? 'center'};
-  align-items: ${props => props.justifyContent ?? 'center'};
+  align-items: ${props => props.alignItems ?? 'center'};
   flex-wrap: ${props => (props.wrap ? 'wrap' : 'nowrap')};
   padding: ${({ padding }) => (padding ? padding : '0px')};
 `
