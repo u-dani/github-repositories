db9f00c2ca1eef3abb7af4d190b671be16bcd3e1
u-dani <danibezsouza@gmail.com>
2023-09-27 11:51:33 -0300

    set routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+import App from './App.tsx'
+import { UserPage } from './pages/UserPage.tsx'
+import { RepositoriesPage } from './pages/repositoriesPage.tsx'
+import { ErrorPage } from './pages/ErrorPage.tsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/usuario/:user',
+    element: <UserPage />,
+  },
+  { path: '/repositorios', element: <RepositoriesPage /> },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
