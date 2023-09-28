5dbc4fac6f227719cbed2f731bb74b4c96407619
u-dani <danibezsouza@gmail.com>
2023-09-27 21:25:20 -0300

    search for user data with the searchUser function

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { searchUser } from '../services/searchUser'
+
 export const UserPage = () => {
-  return <main>pagina do usuario</main>
+  const { user } = useParams()
+  const [userData, setUserData] = useState()
+
+  useEffect(() => {
+    async function r() {
+      const data = await searchUser(user ?? '')
+      setUserData(data)
+    }
+    r()
+  }, [user])
+
+  return (
+    <main>
+      <h1>Página do usuario</h1>
+    </main>
+  )
 }
