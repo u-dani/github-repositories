e6cd78b80137d2a95d3727545b294ff21533262f
u-dani <danibezsouza@gmail.com>
2023-09-29 18:20:49 -0300

    change color of input when focusing

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -20,11 +20,13 @@ export const Input = styled.input<InputProps>`
   height: ${props => props.height || 'auto'};
   max-width: ${props => props.maxWidth || 'auto'};
   background: transparent;
-  border: 1px solid rgba(250, 250, 250, 0.4);
+  border: 2px solid transparent;
+  outline: 1px solid rgba(250, 250, 250, 0.4);
 
   &:focus {
     background: transparent;
-    border-color: rgba(255, 255, 255, 0.8);
+    border-color: #58a6ff;
+    outline-color: transparent;
     font-weight: normal;
   }
 `
