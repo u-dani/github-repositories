d2b598e8c412b24b192bc4e117bf4aa7d688caf4
u-dani <danibezsouza@gmail.com>
2023-09-29 15:36:13 -0300

    adding type

--- src/services/searchUser.ts
+++ src/services/searchUser.ts
@@ -1,6 +1,20 @@
 import { api } from "./api"
 
-export const searchUser = async(username: string) => {
+interface ISearchUserResponse {
+    avatar_url: string
+    bio: string
+    blog: string
+    company: string
+    followers: string
+    followers_url: string
+    following: string
+    following_url: string
+    html_url: string
+    id: string
+    location: string
+}
+
+export const searchUser = async(username: string): Promise<ISearchUserResponse> => {
     const { data } = await api.get(`/users/${username}`)
     return data
 }
