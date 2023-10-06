652d1b2230a4e80e44ef84ad4fea80dd136c0bbb
u-dani <danibezsouza@gmail.com>
2023-10-06 16:07:41 -0300

    Header created and `ButtonFilter` now controls the display of filters

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -15,34 +15,6 @@ import { useParams } from 'react-router-dom'
 import { Input } from '../components/form/Input'
 import { Select } from '../components/form/Select'
 
-const ButtonFilter = styled(Button)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 4px;
-
-  #ishow-filters {
-    all: unset;
-    position: absolute;
-    inset: 0;
-    cursor: pointer;
-    z-index: 50;
-  }
-
-  &:has(#ishow-filters:checked) {
-    background-color: transparent;
-    background-position: 0 0;
-    color: #58a6ff;
-  }
-`
-
-const WrapperRepositories = styled.div`
-  display: grid;
-  gap: 16px;
-  grid-template-columns: 1fr 1fr;
-  width: 100%;
-`
-
 export const UserPage = () => {
   const { user: username } = useParams()
   const [isLoading, setIsLoading] = useState(true)
@@ -99,40 +71,51 @@ export const UserPage = () => {
       </WrapperFlex>
 
       <WrapperFlex direction='column' gap='16px'>
-        <WrapperFlex gap='16px' justifyContent='space-between'>
-          <SearchForm
-            WrapperFlexProps={{ width: '100%', maxWidth: '700px' }}
-            SelectProps={{
-              id: 'search-form',
-              width: '160px',
-            }}
-          />
-          <ButtonFilter width='100px' height='35px'>
-            <input type='checkbox' name='show-filters' id='ishow-filters' />
-            <ListFilter size={20} strokeWidth={2.5} />
-            <Text weight='bold' size='sm'>
-              Filtros
-            </Text>
-          </ButtonFilter>
-        </WrapperFlex>
-
-        <WrapperFlex style={{ background: '#222' }}>
-          <Input placeholder='Busque por um repositório' />
-
-          {filteredLanguages.length > 1 && (
-            <Select
-              id='select-filter-by-language'
-              placeholder='Linguagem'
-              options={filteredLanguages}
-              selectedValue={languageSelectedValue}
-              handleSelect={lang => {
-                lang === 'Tudo'
-                  ? setLanguageSelectedValue(undefined)
-                  : setLanguageSelectedValue(lang)
+        <Header direction='column' gap='16px'>
+          <WrapperFlex gap='16px' justifyContent='space-between'>
+            <SearchForm
+              WrapperFlexProps={{
+                width: '100%',
+                maxWidth: '700px',
+                gap: '16px',
+              }}
+              SelectProps={{
+                id: 'search-form',
+                width: '160px',
               }}
             />
-          )}
-        </WrapperFlex>
+            <ButtonFilter width='100px' height='35px'>
+              <input type='checkbox' name='show-filters' id='ishow-filters' />
+              <ListFilter size={20} strokeWidth={2.5} />
+              <Text weight='bold' size='sm'>
+                Filtros
+              </Text>
+            </ButtonFilter>
+          </WrapperFlex>
+
+          <WrapperFilters
+            padding='8px'
+            gap='16px'
+            justifyContent='start'
+            id='oi'>
+            <Input placeholder='Busque por um repositório' width='100%' />
+
+            {filteredLanguages.length > 1 && (
+              <Select
+                width='30%'
+                id='select-filter-by-language'
+                placeholder='Linguagem'
+                options={filteredLanguages}
+                selectedValue={languageSelectedValue}
+                handleSelect={lang => {
+                  lang === 'Tudo'
+                    ? setLanguageSelectedValue(undefined)
+                    : setLanguageSelectedValue(lang)
+                }}
+              />
+            )}
+          </WrapperFilters>
+        </Header>
 
         <WrapperRepositories>
           {reposData?.map(data => (
@@ -143,3 +126,46 @@ export const UserPage = () => {
     </WrapperFlex>
   )
 }
+
+const WrapperFilters = styled(WrapperFlex)`
+  background-color: #222;
+  border-radius: 4px;
+`
+
+const Header = styled(WrapperFlex)`
+  ${WrapperFilters} {
+    display: none;
+  }
+
+  &:has(#ishow-filters:checked) ${WrapperFilters} {
+    display: flex;
+  }
+`
+
+const ButtonFilter = styled(Button)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+
+  #ishow-filters {
+    all: unset;
+    position: absolute;
+    inset: 0;
+    cursor: pointer;
+    z-index: 50;
+  }
+
+  &:has(#ishow-filters:checked) {
+    background-color: transparent;
+    background-position: 0 0;
+    color: #58a6ff;
+  }
+`
+
+const WrapperRepositories = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: 1fr 1fr;
+  width: 100%;
+`
