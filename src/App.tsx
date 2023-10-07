b08ad9c128e14a582f23dc5d534e73544e329024
u-dani <danibezsouza@gmail.com>
2023-10-06 22:24:18 -0300

    adding <Outlet/> and setting the maximum page width to 1300px

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,12 @@
-import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import { WrapperFlex } from './components/layout/WrapperFlex'
-import { Text } from './components/Text'
-import { Message } from './components/Message'
-import { IMessageProps } from './components/Message'
-import { SearchForm } from './components/form/SearchForm'
-import { Link } from './components/Link'
 
 function App() {
-  const [messageData, setMessageData] = useState<IMessageProps>({
-    message: 'Mensagem aqui :D',
-    state: 'error',
-    show: false,
-    handleClose: () => {
-      setMessageData({ ...messageData, show: false })
-    },
-    timeOut: 3000,
-  })
-
-  const handleErrorForm = (message: string) => {
-    setMessageData({ ...messageData, message: message, show: true })
-  }
-
   return (
-    <WrapperFlex
-      direction='column'
-      gap='40px'
-      height='100vh'
-      padding='40px'
-      width='100vw'
-      maxWidth='800px'
-      position='relative'>
-      <Message {...messageData} />
-
-      <Text as='h1' size='xxl' align='center' weight='bold'>
-        Repositórios do Github
-      </Text>
-
-      <SearchForm
-        handleError={handleErrorForm}
-        InputProps={{ height: '45px', fontSize: '1rem' }}
-        SelectProps={{
-          id: 'select-search-form-app',
-          height: '45px',
-          fontSize: '1rem',
-        }}
-      />
-
-      <Text size='sm' color='gray'>
-        Projeto feito por{' '}
-        <Link to='https://github.com/u-dani' target='_blank'>
-          u-dani
-        </Link>
-      </Text>
+    <WrapperFlex>
+      <WrapperFlex maxWidth='1300px'>
+        <Outlet />
+      </WrapperFlex>
     </WrapperFlex>
   )
 }
