48f152f6c500cc87c62577a7227053c96bf5ae7c
u-dani <danibezsouza@gmail.com>
2023-10-07 13:30:24 -0300

    slicing the repositories array

--- src/components/RepositoryContainer.tsx
+++ src/components/RepositoryContainer.tsx
@@ -1,41 +1,33 @@
+import styled from 'styled-components'
 import { ISearchUserRepositories } from '../services/searchUserRepositories'
 import { RepositoryCard } from './RepositoryCard'
-import styled from 'styled-components'
 import { useSearchParams } from 'react-router-dom'
-import { WrapperFlex } from './layout/WrapperFlex'
 
 interface IRepositoryContainerProps {
   repos: ISearchUserRepositories[]
+  perPage?: number
 }
 
-const reposPerPage = 20
+export const RepositoryContainer = ({
+  repos,
+  perPage,
+}: IRepositoryContainerProps) => {
+  const [searchParams] = useSearchParams()
+  const page = Number(searchParams.get('page')) || 1
 
-export const RepositoryContainer = ({ repos }: IRepositoryContainerProps) => {
-  const [searchParams, setSearchParams] = useSearchParams()
+  const reposPerPage = perPage ?? 20
 
-  const previousPage = () => {
-    console.log('prev page')
-  }
-
-  const nextPage = () => {
-    console.log('next page')
-  }
-
-  const repositoryArraySlice = repos.slice(0, reposPerPage)
+  const repositoryArraySlice = repos.slice(
+    reposPerPage * (page - 1),
+    reposPerPage * page
+  )
 
   return (
-    <WrapperFlex gap='24px' direction='column'>
-      <WrapperRepositories>
-        {repositoryArraySlice.map(data => (
-          <RepositoryCard key={data.id} {...data} />
-        ))}
-      </WrapperRepositories>
-
-      <WrapperFlex gap='16px'>
-        <button onClick={previousPage}>Anterior</button>
-        <button onClick={nextPage}>Próxima</button>
-      </WrapperFlex>
-    </WrapperFlex>
+    <WrapperRepositories>
+      {repositoryArraySlice.map(data => (
+        <RepositoryCard key={data.id} {...data} />
+      ))}
+    </WrapperRepositories>
   )
 }
 
