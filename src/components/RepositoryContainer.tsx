3c47d34774fe3d668b4f3e4b20c353f6a84633f1
u-dani <danibezsouza@gmail.com>
2023-10-13 13:33:19 -0300

    shows message if repositoryArraySlice is empty

--- src/components/RepositoryContainer.tsx
+++ src/components/RepositoryContainer.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
+import { Frown } from 'lucide-react'
 import { ISearchUserRepositories } from '../services/searchUserRepositories'
 import { RepositoryCard } from './RepositoryCard'
+import { Text } from './Text'
+import { WrapperFlex } from './layout/WrapperFlex'
 import { useSearchParams } from 'react-router-dom'
 
 interface IRepositoryContainerProps {
@@ -23,11 +26,20 @@ export const RepositoryContainer = ({
   )
 
   return (
-    <WrapperRepositories>
-      {repositoryArraySlice.map(data => (
-        <RepositoryCard key={data.id} {...data} />
-      ))}
-    </WrapperRepositories>
+    <>
+      {repositoryArraySlice.length === 0 ? (
+        <WrapperFlex direction='column' gap='4px' height='240px'>
+          <Frown size={32} style={{ color: 'gold' }} />
+          <Text>Não há repositórios públicos</Text>
+        </WrapperFlex>
+      ) : (
+        <WrapperRepositories>
+          {repositoryArraySlice.map(data => (
+            <RepositoryCard key={data.id} {...data} />
+          ))}
+        </WrapperRepositories>
+      )}
+    </>
   )
 }
 
