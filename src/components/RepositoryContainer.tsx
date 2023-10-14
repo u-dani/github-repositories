3d2551602b7b74007a6f47b53d97794bbdafdedd
u-dani <danibezsouza@gmail.com>
2023-10-14 04:10:44 -0300

    removing multiple responsibilities, shows untreated repositories

--- src/components/RepositoryContainer.tsx
+++ src/components/RepositoryContainer.tsx
@@ -1,43 +1,28 @@
 import styled from 'styled-components'
 import { Frown } from 'lucide-react'
-import { ISearchUserRepositories } from '../services/searchUserRepositories'
+import { ISearchUserRepositoriesResponse } from '../services/searchUserRepositories'
 import { RepositoryCard } from './RepositoryCard'
 import { Text } from './Text'
 import { WrapperFlex } from './layout/WrapperFlex'
-import { useSearchParams } from 'react-router-dom'
-
-interface IRepositoryContainerProps {
-  repos: ISearchUserRepositories[]
-  perPage?: number
-}
 
 export const RepositoryContainer = ({
   repos,
-  perPage,
-}: IRepositoryContainerProps) => {
-  const [searchParams] = useSearchParams()
-  const page = Number(searchParams.get('page')) || 1
-
-  const reposPerPage = perPage ?? 20
-
-  const repositoryArraySlice = repos.slice(
-    reposPerPage * (page - 1),
-    reposPerPage * page
-  )
-
+}: {
+  repos: ISearchUserRepositoriesResponse[] | undefined
+}) => {
   return (
     <>
-      {repositoryArraySlice.length === 0 ? (
+      {repos ? (
+        <WrapperRepositories>
+          {repos.map(data => (
+            <RepositoryCard key={data.id} {...data} />
+          ))}
+        </WrapperRepositories>
+      ) : (
         <WrapperFlex direction='column' gap='4px' height='240px'>
           <Frown size={32} style={{ color: 'gold' }} />
           <Text>Não há repositórios públicos</Text>
         </WrapperFlex>
-      ) : (
-        <WrapperRepositories>
-          {repositoryArraySlice.map(data => (
-            <RepositoryCard key={data.id} {...data} />
-          ))}
-        </WrapperRepositories>
       )}
     </>
   )
