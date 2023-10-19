a2deac28e5190a9ccb5340b1704d9db079cdb9e7
u-dani <danibezsouza@gmail.com>
2023-10-19 14:43:15 -0300

    encodeURI language

--- src/services/searchRepositories.ts
+++ src/services/searchRepositories.ts
@@ -22,14 +22,15 @@ export interface ISearchRepositoriesProps {
 }
 
 export const searchRepositories = async ({page = 1, per_page = 20, ...props}: ISearchRepositoriesProps) => {
-    const language = props.language ? `+language%3A${props.language}` : ''
+
+    const codedLanguage = encodeURIComponent(props.language ?? '')
+    const language = codedLanguage ? `+language%3A${codedLanguage}` : ''
     const license = props.license ? `+license%3A${licenses[props.license].parameter}` : ''
     const forks = props.numberOfForks ? `+forks%3A${props.numberOfForks}` : ''
     const stars = props.numberOfStars ? `+stars%3A${props.numberOfStars}` : ''
     const topic = props.topic ? props.topic.reduce((acc, cv) => acc + `+topic%3A${cv}` , '') : ''
 
     const searchParams = `q=${props.query}${language}${license}${forks}${stars}${topic}&page=${page}&per_page=${per_page}`
-
     const { data } = await api.get(`/search/repositories?${searchParams}`)
     return data
 }
